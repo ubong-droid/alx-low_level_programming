85a35f9d49f06db9f3212bbfa8a1c9213eaa1144
ubong-droid <121963283+ubong-droid@users.noreply.github.com>
2023-02-20 23:08:28 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -8,6 +7,5 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
